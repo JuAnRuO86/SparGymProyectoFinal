c594ef2406de4075b37020ad976935f22c4746ff
Juan Ruiz <juaanruiiz86@gmail.com>
2023-06-07 09:52:06 +0200

    nuevo despliegue

--- resources/util/databases.php
+++ resources/util/databases.php
@@ -1,8 +1,8 @@
 <?php
-    $servidor = 'containers-us-west-141.railway.app';
+    $servidor = 'containers-us-west-137.railway.app';
     $usuario = 'root';
-    $contrasena = 'Syhksl3FPwKjLcuaOUER';
+    $contrasena = 'FG1bX4UaLfItFp3gAge0';
     $base_de_datos = 'railway';
-    $port = '6783'; 
+    $port = '7045'; 
     $conexion = new mysqli($servidor, $usuario, $contrasena, $base_de_datos, $port) or die("Error en la conexion");
 ?>
